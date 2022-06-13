fc32678020e85357cc6b3f34419910260720f068
NATH AiT <blackait167@gmail.com>
2022-06-13 17:48:44 +0700

    Add project files.

---
+++ BooksStore/Models/SeedData.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            BooksStoreDbContext context =
+            app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService < BooksStoreDbContext > ();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Books.Any())
+            {
+                context.Books.AddRange(
+               new Book
+               {
+                   Title = "Samsung Galaxy S22 Ultra 5G",
+                   Description = "https://cdn.tgdd.vn/Products/Images/42/235838/Galaxy-S22-Ultra-Burgundy-600x600.jpg",
+                   Genre = "samsung",
+                   Price = 28m
+               },
+                new Book
+                {
+                    Title = "iPhone 11",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/153856/iphone-xi-vang-600x600.jpg",
+                    Genre = "Iphone",
+                    Price = 13m
+                },
+                new Book
+                {
+                    Title = "OPPO Reno7 series",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/271717/oppo-reno7-z-5g-thumb-1-1-600x600.jpg",
+                    Genre = "OPPO",
+                    Price = 10m
+                },
+                new Book
+                {
+                    Title = "Xiaomi 11T",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/248293/TimerThumb/xiaomi-11t-(8).jpg",
+                    Genre = "Xiaomi",
+                    Price = 10m
+                },
+                new Book
+                {
+                    Title = "Samsung Galaxy A03",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/251856/samsung-galaxy-a03-xanh-thumb-600x600.jpg",
+                    Genre = "Samsung",
+                    Price = 2m
+                },
+                new Book
+                {
+                    Title = "Xiaomi Redmi Note 11 Series",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/270471/xiaomi-redmi-note-11-pro-trang-thumb-600x600.jpg",
+                    Genre = "Xiaomi",
+                    Price = 7m
+                },
+                new Book
+                {
+                    Title = "Xiaomi Redmi Note 11S series",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/272668/xiaomi-redmi-note-11s-5g-%20thumb-600x600.jpg",
+                    Genre = "Xiaomi",
+                    Price = 6m
+                },
+                new Book
+                {
+                    Title = "iPhone 13 Pro Max",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/230529/iphone-13-pro-max-gold-1-600x600.jpg",
+                    Genre = "Iphone",
+                    Price = 31m
+                },
+                new Book
+                {
+                    Title = "Samsung Galaxy A52s",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/247507/TimerThumb/samsung-galaxy-a52s-5g-(8).jpg",
+                    Genre = "Samsung",
+                    Price = 7m
+                },
+                new Book
+                {
+                    Title = "OPPO A55",
+                    Description = "https://cdn.tgdd.vn/Products/Images/42/249944/TimerThumb/oppo-a55-4g-(6).jpg",
+                    Genre = "OPPO",
+                    Price = 4m
+                }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,43 @@
+# Books-Store
+trang chủ website
+![image](https://user-images.githubusercontent.com/74332737/173279086-87cabd8a-d791-452c-9aaa-1e3b972cb40c.png)
+trang giỏ hàng 
+![image](https://user-images.githubusercontent.com/74332737/173276925-17828ba1-7044-4598-896c-7abb9e41787f.png)
+trang đặt hàng
+![image](https://user-images.githubusercontent.com/74332737/173276975-f7ffbc42-6ac2-4a4e-b49c-1d4e03fd5998.png)
+# các tính năng của web
+1- tính năng phân loại từng cuốn sách 
+![image](https://user-images.githubusercontent.com/74332737/173279168-77fef498-b973-4377-88b7-cb7487d126b8.png)
+- giúp phân loại sách theo từng thể loại tìm kiếm
+- ![image](https://user-images.githubusercontent.com/74332737/173279270-0bf23ceb-c351-4fff-ac47-4c2e0e78ccfd.png)
+2- tính năng thêm sách vào giỏ hàng
+![image](https://user-images.githubusercontent.com/74332737/173279359-8cc93c6b-8088-4ff8-93e2-d1fd4290ede9.png)
+- khi nhấn nút add to cart thì sách sẻ được thêm vào giỏ hàng 
+- ![image](https://user-images.githubusercontent.com/74332737/173277422-93082b64-ade0-4a96-9fa1-468155e81f0f.png)
+= ![image](https://user-images.githubusercontent.com/74332737/173277473-6bda654e-a1c4-4d7b-8eed-651d0d1559b4.png)
+3 tính năng remote giúp loại bỏ những cuốn sách không muốn khỏi giỏ hàng 
+![image](https://user-images.githubusercontent.com/74332737/173277598-972e60c9-de07-414d-9c83-e6635fbded15.png)
+![image](https://user-images.githubusercontent.com/74332737/173277618-8d3cf2ca-8724-4a22-ad2c-5ba78ac74d0c.png)
+4 tính năng checkout để đặt hàng và mua sản phẩm
+![image](https://user-images.githubusercontent.com/74332737/173277725-405a6688-7086-4f0c-a31e-d9e428e5eafd.png)
+5 trang Admin để kiểm tra cài đặt và Order khi truy cập https://localhost:44347/admin
+- phần Books liệt kê những cuốn sách đang có trong cửa hàng
+![image](https://user-images.githubusercontent.com/74332737/173278124-2f9bce99-cee1-4922-a40b-f45b24345979.png)
+
+- nhấn Details để xem thông tin cuốn sách
+
+![image](https://user-images.githubusercontent.com/74332737/173278571-855e66c1-5978-4a73-a1c2-18b44dfba65a.png)
+
+- nhấn Edit để chỉnh sửa thông tin cuốn sách 
+
+![image](https://user-images.githubusercontent.com/74332737/173278650-4fc803c4-076a-4e87-93ef-0d32532aaf2e.png)
+
+- và nhấn Delete để xóa cuốn sách khỏi cửa hàng 
+
+![image](https://user-images.githubusercontent.com/74332737/173278742-ecc5eac3-dfaa-4f32-8135-93b2bd98acb7.png)
+
+
+- Phần Order liệt kê cuốn sách đã được khách đặt mua
+![image](https://user-images.githubusercontent.com/74332737/173278187-a2ba4a93-055c-4366-8a3f-96efb39f835e.png)
+
+
